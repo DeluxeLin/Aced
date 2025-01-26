29f5a2d46bad3bea03ff102628794ae77f2abbeb
DeluxeLin <DeluxeLin@outlook.com>
2025-01-26 16:26:43 +0800

    优化: 将gui中不可存放的槽位挡住

--- src/main/java/top/lingcar4870/aced/client/gui/DeathInheritGUI.java
+++ src/main/java/top/lingcar4870/aced/client/gui/DeathInheritGUI.java
@@ -12,6 +12,7 @@ import javax.annotation.ParametersAreNonnullByDefault;
 @ParametersAreNonnullByDefault
 public class DeathInheritGUI extends AbstractContainerScreen<DeathInheritContainerMenu> {
     private static final ResourceLocation CONTAINER_BACKGROUND = new ResourceLocation("textures/gui/container/generic_54.png");
+    public static final ResourceLocation ILLEGAL_SLOTS = new ResourceLocation("aced:textures/gui/container/illegal_slots.png");
     private final int containerRows;
 
     public DeathInheritGUI(DeathInheritContainerMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
@@ -25,6 +26,12 @@ public class DeathInheritGUI extends AbstractContainerScreen<DeathInheritContain
     public void render(GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick) {
         this.renderBackground(pGuiGraphics);
         super.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
+        int i = (this.width - this.imageWidth) / 2;
+        int j = (this.height - this.imageHeight) / 2;
+        int n = this.menu.getContainerSize() % 9;
+        if (n != 0) {
+            pGuiGraphics.blit(ILLEGAL_SLOTS, i + n * 18 + 7, j + this.containerRows * 18 - 1, 0, 0, 18 * (9 - n), 18);
+        }
         this.renderTooltip(pGuiGraphics, pMouseX, pMouseY);
     }
 
